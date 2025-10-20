c70f78a5d2ddb331dadb746445a4f9ea3f38872b
Mina <S1mM1n6h@gmail.com>
2025-10-21 08:35:12 +0900

    [Silver IV] Title: 제로, Time: 216 ms, Memory: 23956 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,24 +1,28 @@
-import java.util.Scanner;
-import java.util.Stack;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        Stack<Integer> stack=new Stack<>();
-        for(int i=1;i<=n;i++){
-            int num=sc.nextInt();
-            if(num==0)
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int K = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = 0; i < K; i++){
+            int num = Integer.parseInt(br.readLine());
+
+            if(num==0){
                 stack.pop();
-            else
+            }
+            else{
                 stack.push(num);
+            }
         }
-        int sum = 0;
-        for (int i : stack) {
-            sum += i;
+        int sum=0;
+        for(int num:stack){
+            sum+=num;
         }
-
         System.out.println(sum);
-        sc.close();
     }
 }
