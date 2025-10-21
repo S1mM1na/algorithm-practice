563e27240a9fbbf228c050788ea64e4f515a2605
Mina <S1mM1n6h@gmail.com>
2025-10-21 17:06:05 +0900

    [Silver IV] Title: 제로, Time: 208 ms, Memory: 23928 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -2,27 +2,26 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int K = Integer.parseInt(br.readLine());
         Stack<Integer> stack = new Stack<>();
+        int sum = 0;
 
         for(int i = 0; i < K; i++){
             int num = Integer.parseInt(br.readLine());
 
-            if(num==0){
-                stack.pop();
+            if(num == 0){
+                sum -= stack.pop();
             }
             else{
                 stack.push(num);
+                sum += num;
             }
         }
-        int sum=0;
-        for(int num:stack){
-            sum+=num;
-        }
+        
         System.out.println(sum);
     }
 }
